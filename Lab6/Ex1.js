cb26f200fbea31e6bb64bb5bd79b8522f4d7578d
momoka1926 <ms295@hawaii.edu>
2021-09-23 17:03:39 +0900

    Uploaded my resubmission

---
+++ Lab6/Ex1.js
@@ -0,0 +1,8 @@
+var age =  20;
+var fav_num = 2;
+var day_of_birth = 26;
+var month_of_birth = 1;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
