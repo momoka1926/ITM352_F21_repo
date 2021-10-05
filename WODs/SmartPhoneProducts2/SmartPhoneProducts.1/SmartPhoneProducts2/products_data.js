e8c8d1b937d7505c330ff086a9312546898c6198
momoka1926 <ms295@hawaii.edu>
2021-10-05 14:12:43 +0900

    Updated

--- WODs/SmartPhoneProducts2/SmartPhoneProducts.1/SmartPhoneProducts2/products_data.js
+++ WODs/SmartPhoneProducts2/SmartPhoneProducts.1/SmartPhoneProducts2/products_data.js
@@ -1,6 +1,6 @@
 console.log("Product variables loaded...");
-     num_items = 5;
-     var name1 = "HTC";
+
+var name1 = "HTC";
      var price1 = 40.00;
      var image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
 
@@ -19,5 +19,3 @@ console.log("Product variables loaded...");
      var name5 = "Blackberry";
      var price5 = 10.00;
      var image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
-
-
